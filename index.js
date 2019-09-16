9d99c8b564fbf229b7c99b8260e1839cd5796c26
Jake Navarro <jt10navarro@gmail.com>
2019-09-16 11:45:01 -0500

    declared variables for dice and created 2 alerts for Player 1 - roll dice & first output

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-use strict;
+function rollTheDice(){
+	alert("Player 1 roll the dice!");
+	let die1 = Math.floor( Math.random() * 4 ) +1;
+	let die2 = Math.floor( Math.random() * 6 ) +1;
+	let die3 = Math.floor( Math.random() * 8 ) +1;
+	let die4 = Math.floor( Math.random() * 10 ) +1;
+	let die5 = Math.floor( Math.random() * 12 ) +1;
+	let die6 = Math.floor( Math.random() * 20 ) +1;
+	let diceTotal = [die1, die2, die3, die4, die5, die6];
+	let player1Counter = 0;
+	let player2Counter = 0;
+	let player3Counter = 0;
+	alert('You rolled ' + diceTotal);
+}
 
+rollTheDice()
